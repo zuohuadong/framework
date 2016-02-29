944779cae2babd9cb1bf544806873d388d5c9a0a
twilroad <269044570@qq.com>
2016-02-29 15:34:09 +0800

    添加InjectPage的trait

---
+++ src/Notadd/Foundation/Traits/InjectPageTrait.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2015, iBenchu.org
+ * @datetime 2016-02-29 15:31
+ */
+namespace Notadd\Foundation\Traits;
+use Illuminate\Container\Container;
+/**
+ * Class InjectPageTrait
+ * @package Notadd\Foundation\Traits
+ */
+class InjectPageTrait {
+    /**
+     * @var \Notadd\Page\Factory
+     */
+    private $page;
+    /**
+     * @return \Notadd\Page\Factory
+     */
+    public function getPage() {
+        if(!isset($this->page)) {
+            $this->page = Container::getInstance()->make('page');
+        }
+        return $this->page;
+    }
+}
