8b394ec0e9bd92a1001733c8bda84ed3f30b44b2
twilroad <269044570@qq.com>
2016-03-02 20:19:42 +0800

    解决在Http中异常信息不显示而显示500的bug

--- src/Notadd/Foundation/Exceptions/Handler.php
+++ src/Notadd/Foundation/Exceptions/Handler.php
@@ -7,11 +7,18 @@
  */
 namespace Notadd\Foundation\Exceptions;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Exception\HttpResponseException;
+use Illuminate\Validation\ValidationException;
 use Psr\Log\LoggerInterface;
 use Illuminate\Http\Response;
+use Symfony\Component\Debug\Exception\FlattenException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Console\Application as ConsoleApplication;
-use Symfony\Component\Debug\ExceptionHandler as SymfonyDisplayer;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
 use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
 /**
  * Class Handler
@@ -25,7 +32,12 @@ class Handler implements ExceptionHandlerContract {
     /**
      * @var array
      */
-    protected $dontReport = [];
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
+    ];
     /**
      * @param \Psr\Log\LoggerInterface $log
      */
@@ -53,7 +65,8 @@ class Handler implements ExceptionHandlerContract {
      * @return bool
      */
     protected function shouldntReport(Exception $e) {
-        foreach($this->dontReport as $type) {
+        $dontReport = array_merge($this->dontReport, [HttpResponseException::class]);
+        foreach($dontReport as $type) {
             if($e instanceof $type) {
                 return true;
             }
@@ -66,6 +79,15 @@ class Handler implements ExceptionHandlerContract {
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $e) {
+        if($e instanceof HttpResponseException) {
+            return $e->getResponse();
+        } elseif($e instanceof ModelNotFoundException) {
+            $e = new NotFoundHttpException($e->getMessage(), $e);
+        } elseif($e instanceof AuthorizationException) {
+            $e = new HttpException(403, $e->getMessage());
+        } elseif($e instanceof ValidationException && $e->getResponse()) {
+            return $e->getResponse();
+        }
         if($this->isHttpException($e)) {
             return $this->toIlluminateResponse($this->renderHttpException($e), $e);
         } else {
@@ -97,7 +119,7 @@ class Handler implements ExceptionHandlerContract {
     protected function renderHttpException(HttpException $e) {
         $status = $e->getStatusCode();
         if(view()->exists("errors.{$status}")) {
-            return response()->view("errors.{$status}", ['exception' => $e], $status);
+            return response()->view("errors.{$status}", ['exception' => $e], $status, $e->getHeaders());
         } else {
             return $this->convertExceptionToResponse($e);
         }
@@ -107,7 +129,36 @@ class Handler implements ExceptionHandlerContract {
      * @return \Symfony\Component\HttpFoundation\Response
      */
     protected function convertExceptionToResponse(Exception $e) {
-        return (new SymfonyDisplayer(config('app.debug')))->createResponse($e);
+        $e = FlattenException::create($e);
+        $handler = new SymfonyExceptionHandler(config('app.debug'));
+        $decorated = $this->decorate($handler->getContent($e), $handler->getStylesheet($e));
+        return SymfonyResponse::create($decorated, $e->getStatusCode(), $e->getHeaders());
+    }
+    /**
+     * @param $content
+     * @param $css
+     * @return string
+     */
+    protected function decorate($content, $css) {
+        return <<<EOF
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="robots" content="noindex,nofollow" />
+        <style>
+            /* Copyright (c) 2010, Yahoo! Inc. All rights reserved. Code licensed under the BSD License: http://developer.yahoo.com/yui/license.html */
+            html{color:#000;background:#FFF;}body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,code,form,fieldset,legend,input,textarea,p,blockquote,th,td{margin:0;padding:0;}table{border-collapse:collapse;border-spacing:0;}fieldset,img{border:0;}address,caption,cite,code,dfn,em,strong,th,var{font-style:normal;font-weight:normal;}li{list-style:none;}caption,th{text-align:left;}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal;}q:before,q:after{content:'';}abbr,acronym{border:0;font-variant:normal;}sup{vertical-align:text-top;}sub{vertical-align:text-bottom;}input,textarea,select{font-family:inherit;font-size:inherit;font-weight:inherit;}input,textarea,select{*font-size:100%;}legend{color:#000;}
+            html { background: #eee; padding: 10px }
+            img { border: 0; }
+            #sf-resetcontent { width:970px; margin:0 auto; }
+            $css
+        </style>
+    </head>
+    <body>
+        $content
+    </body>
+</html>
+EOF;
     }
     /**
      * @param \Exception $e
