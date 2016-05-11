6e213fd81ea528075c8bd7ee7e9200ca173b9d20
twilroad <269044570@qq.com>
2016-05-11 17:49:21 +0800

    添加Configuration、List两个控制器及对应的首页模板

--- src/Notadd/Attachment/AttachmentServiceProvider.php
+++ src/Notadd/Attachment/AttachmentServiceProvider.php
@@ -8,6 +8,8 @@
 namespace Notadd\Attachment;
 use Illuminate\Support\ServiceProvider;
 use Notadd\Attachment\Controllers\Admin\AttachmentController;
+use Notadd\Attachment\Controllers\Admin\ConfigurationController;
+use Notadd\Attachment\Controllers\Admin\ListController;
 use Notadd\Foundation\Traits\InjectEventsTrait;
 use Notadd\Foundation\Traits\InjectRouterTrait;
 /**
@@ -21,6 +23,8 @@ class AttachmentServiceProvider extends ServiceProvider {
      */
     public function boot() {
         $this->getRouter()->group(['middleware' => 'auth.admin', 'prefix' => 'admin'], function() {
+            $this->getRouter()->resource('attachment/list', ListController::class);
+            $this->getRouter()->resource('attachment/configuration', ConfigurationController::class);
             $this->getRouter()->resource('attachment', AttachmentController::class);
         });
     }
--- views/admin/attachment/configuration.blade.php
+++ views/admin/attachment/configuration.blade.php
@@ -5,18 +5,15 @@
         <ol class="breadcrumb breadcrumb-small">
             <li>后台首页</li>
             <li><a href="{{ url('admin/attachment') }}">附件管理</a></li>
+            <li><a href="{{ url('admin/attachment/configuration') }}">附件配置</a></li>
         </ol>
         <div class="page-wrap">
             <div class="row">
                 <div class="col-md-12">
                     <div class="panel panel-lined clearfix mb30">
-                        <div class="panel-heading"><i>页面：附件管理 - 功能选择</i></div>
+                        <div class="panel-heading"><i>页面：附件配置</i></div>
                         <div class="row mt20">
-                            <div class="col-md-4 col-md-offset-4">
-                                <div class="list-group">
-                                    <a href="{{ url('admin/attachment/configuration') }}" class="list-group-item">附件配置</a>
-                                    <a href="{{ url('admin/attachment/list') }}" class="list-group-item">附件列表</a>
-                                </div>
+                            <div class="col-md-12">
                             </div>
                         </div>
                     </div>
