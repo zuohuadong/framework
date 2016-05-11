c867a3bc0fe78d4633e2c5c505692cdae8e47fd3
twilroad <269044570@qq.com>
2016-05-11 12:32:39 +0800

    添加Attachment组件的基本结构

---
+++ src/Notadd/Attachment/AttachmentServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2015, iBenchu.org
+ * @datetime 2016-05-11 12:26
+ */
+namespace Notadd\Attachment;
+use Illuminate\Support\ServiceProvider;
+/**
+ * Class AttachmentServiceProvider
+ * @package Notadd\Attachment
+ */
+class AttachmentServiceProvider extends ServiceProvider {
+    /**
+     * @return void
+     */
+    public function boot() {
+    }
+    /**
+     * @return void
+     */
+    public function register() {
+    }
+}
---
+++ src/Notadd/Attachment/Factory.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2015, iBenchu.org
+ * @datetime 2016-05-11 12:31
+ */
+namespace Notadd\Attachment;
+class Factory {
+}
