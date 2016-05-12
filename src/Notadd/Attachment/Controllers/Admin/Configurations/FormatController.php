6e1363a502adcbd59791a52dd6ad1ee4c464c546
twilroad <269044570@qq.com>
2016-05-12 18:00:33 +0800

    实现附件格式配置的逻辑，并删除无关的控制器

--- src/Notadd/Attachment/Controllers/Admin/Configurations/FormatController.php
+++ src/Notadd/Attachment/Controllers/Admin/Configurations/FormatController.php
@@ -17,6 +17,12 @@ class FormatController extends AbstractAdminController {
      * @return \Illuminate\Contracts\View\View
      */
     public function index() {
+        $this->share('allow_image_format', $this->setting->get('attachment.format.allow.image'));
+        $this->share('allow_catcher_format', $this->setting->get('attachment.format.allow.catcher'));
+        $this->share('allow_video_format', $this->setting->get('attachment.format.allow.video'));
+        $this->share('allow_file_format', $this->setting->get('attachment.format.allow.file'));
+        $this->share('allow_manager_image_format', $this->setting->get('attachment.format.allow.manager.image'));
+        $this->share('allow_manager_file_format', $this->setting->get('attachment.format.allow.manager.file'));
         return $this->view('attachment.configuration.format');
     }
     /**
@@ -24,6 +30,12 @@ class FormatController extends AbstractAdminController {
      * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request) {
+        $this->setting->set('attachment.format.allow.image', $request->get('allow_image_format'));
+        $this->setting->set('attachment.format.allow.catcher', $request->get('allow_catcher_format'));
+        $this->setting->set('attachment.format.allow.video', $request->get('allow_video_format'));
+        $this->setting->set('attachment.format.allow.file', $request->get('allow_file_format'));
+        $this->setting->set('attachment.format.allow.manager.image', $request->get('allow_manager_image_format'));
+        $this->setting->set('attachment.format.allow.manager.file', $request->get('allow_manager_file_format'));
         return $this->redirect->back();
     }
 }
--- views/admin/attachment/configuration/format.blade.php
+++ views/admin/attachment/configuration/format.blade.php
@@ -14,6 +14,55 @@
                         <div class="panel-heading"><i>页面：附件格式配置</i></div>
                         <div class="row mt20">
                             <div class="col-md-12">
+                                <form class="form-horizontal col-md-12" action="{{ url('admin/attachment/format') }}" autocomplete="off" method="post">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <div class="form-group form-group-sm">
+                                        <label class="col-md-4 control-label">允许上传的图片格式</label>
+                                        <div class="col-md-4">
+                                            <textarea class="form-control" name="allow_image_format" rows="3">{{ app('request')->old('allow_image_format', $allow_image_format) }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-group-sm">
+                                        <label class="col-md-4 control-label">允许远程下载的文件格式</label>
+                                        <div class="col-md-4">
+                                            <textarea class="form-control" name="allow_catcher_format" rows="3">{{ app('request')->old('allow_catcher_format', $allow_catcher_format) }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-group-sm">
+                                        <label class="col-md-4 control-label">允许上传的视频格式</label>
+                                        <div class="col-md-4">
+                                            <textarea class="form-control" name="allow_video_format" rows="3">{{ app('request')->old('allow_video_format', $allow_video_format) }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-group-sm">
+                                        <label class="col-md-4 control-label">允许上传的文件格式</label>
+                                        <div class="col-md-4">
+                                            <textarea class="form-control" name="allow_file_format" rows="3">{{ app('request')->old('allow_file_format', $allow_file_format) }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-group-sm">
+                                        <label class="col-md-4 control-label">允许管理的图片格式</label>
+                                        <div class="col-md-4">
+                                            <textarea class="form-control" name="allow_manager_image_format" rows="3">{{ app('request')->old('allow_manager_image_format', $allow_manager_image_format) }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-group-sm">
+                                        <label class="col-md-4 control-label">允许管理的文件格式</label>
+                                        <div class="col-md-4">
+                                            <textarea class="form-control" name="allow_manager_file_format" rows="3">{{ app('request')->old('allow_manager_file_format', $allow_manager_file_format) }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-group-sm">
+                                        <label class="col-md-4 control-label">配置说明</label>
+                                        <label class="col-md-4 control-label">使用半角分号分隔每一个格式</label>
+                                    </div>
+                                    <div class="form-group form-group-sm">
+                                        <label class="col-md-4 control-label"></label>
+                                        <div class="col-md-4">
+                                            <button type="submit" class="btn btn-primary btn-sm" style="width: 100%;">提交</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
