bb0750899ec61a982273238e818a7714fd3942f5
twilroad <269044570@qq.com>
2016-05-14 13:04:12 +0800

    添加Image组件的Caches

---
+++ src/Notadd/Foundation/Image/Caches/CachedImage.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2015, iBenchu.org
+ * @datetime 2016-05-14 12:58
+ */
+namespace Notadd\Foundation\Image\Caches;
+use Notadd\Foundation\Image\Contracts\Image as ImageContract;
+/**
+ * Class CachedImage
+ * @package Notadd\Foundation\Image\Caches
+ */
+class CachedImage implements ImageContract {
+    private $resource;
+    private $source;
+    private $finfo;
+    private $closed;
+    /**
+     * CachedImage constructor.
+     * @param null $src
+     */
+    public function __construct($src = null) {
+        $src && $this->load($src);
+    }
+    /**
+     * @return void
+     */
+    public function __destruct() {
+        $this->close();
+    }
+    /**
+     * @param string $source
+     * @return bool|void
+     */
+    public function load($source) {
+        $this->close();
+        $this->source = $source;
+        $this->resource = finfo_open(FILEINFO_MIME_TYPE);
+        $this->finfo = [
+            'mime' => finfo_file($this->resource, $this->source),
+            'lastmod' => filemtime($this->source)
+        ];
+        $this->closed = false;
+    }
+    /**
+     * @return mixed
+     */
+    public function getSource() {
+        return $this->source;
+    }
+    /**
+     * @return mixed
+     */
+    protected function isClosed() {
+        return $this->closed;
+    }
+    /**
+     * @return int
+     */
+    public function getLastModTime() {
+        if($this->isClosed()) {
+            return time();
+        }
+        return $this->finfo['lastmod'];
+    }
+    /**
+     *
+     */
+    public function close() {
+        if($this->isClosed()) {
+            return;
+        }
+        if(is_resource($this->resource)) {
+            finfo_close($this->resource);
+        };
+        $this->source = null;
+        $this->resource = null;
+        $this->finfo = null;
+        $this->closed = true;
+    }
+    /**
+     * @return mixed
+     */
+    public function getMimeType() {
+        if(!$this->isClosed()) {
+            return $this->finfo['mime'];
+        }
+    }
+    /**
+     * @return bool
+     */
+    public function isProcessed() {
+        return false;
+    }
+    /**
+     * @param \Notadd\Foundation\Image\Caches\ResolverInterface $resolver
+     */
+    public function process(ResolverInterface $resolver) {
+        throw new \LogicException(sprintf('calling process() on a cached image is not allowed, called with %s', get_class($resolver)));
+    }
+    /**
+     * @param $quality
+     */
+    public function setQuality($quality) {
+        throw new \LogicException(sprintf('calling setQuality() on a cached image is not allowed, called with %s', get_class($quality)));
+    }
+    /**
+     * @param $format
+     */
+    public function setFileFormat($format) {
+        throw new \LogicException(sprintf('calling setFileFormat() on a cached image is not allowed, called with %s', get_class($format)));
+    }
+    /**
+     * @return string
+     */
+    public function getContents() {
+        return file_get_contents($this->source);
+    }
+    /**
+     * @return mixed
+     */
+    public function getFileFormat() {
+        if($this->isClosed()) {
+            return;
+        }
+        if(!isset($this->finfo['extension'])) {
+            $this->finfo['extension'] = preg_replace([
+                '~image/~',
+                '~.*jpeg~i'
+            ], [
+                '',
+                'jpg'
+            ], $this->getMimeType());
+        }
+        return $this->finfo['extension'];
+    }
+    /**
+     * @return mixed
+     */
+    public function getSourceFormat() {
+        return $this->getFileFormat();
+    }
+    /**
+     * @return mixed
+     */
+    public function getSourceMimeTime() {
+        return $this->getMimeType();
+    }
+}
---
+++ src/Notadd/Foundation/Image/Caches/ImageCache.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2015, iBenchu.org
+ * @datetime 2016-05-14 13:01
+ */
+namespace Notadd\Foundation\Image\Caches;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\NamespacedItemResolver;
+use Notadd\Foundation\Image\Contracts\Cache;
+use Notadd\Foundation\Image\Contracts\Image;
+/**
+ * Class ImageCache
+ * @package Notadd\Foundation\Image\Caches
+ */
+class ImageCache extends NamespacedItemResolver implements Cache {
+    /**
+     * @var array
+     */
+    protected $pool = [];
+    /**
+     * @var string
+     */
+    protected $path;
+    /**
+     * @var \Notadd\Foundation\Image\Contracts\Image
+     */
+    protected $image;
+    /**
+     * ImageCache constructor.
+     * @param \Notadd\Foundation\Image\Contracts\Image $image
+     * @param \Illuminate\Filesystem\Filesystem $files
+     * @param $path
+     * @param int $permission
+     */
+    public function __construct(Image $image, Filesystem $files, $path, $permission = 0777) {
+        $this->image = $image;
+        $this->files = $files;
+        $this->setPath($path, $permission);
+    }
+    /**
+     * @param $key
+     * @param bool $raw
+     * @return \Notadd\Foundation\Image\Caches\ImageInterface|\Thapp\JitImage\ImageInterface
+     */
+    public function get($key, $raw = false) {
+        if($this->has($key)) {
+            $this->image->close();
+            $this->image->load($this->pool[$key]);
+            return $raw ? $this->image->getImageBlob() : $this->image;
+        }
+    }
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function has($key) {
+        if(array_key_exists($key, $this->pool)) {
+            return true;
+        }
+        if($this->files->exists($path = $this->getPath($key))) {
+            $this->pool[$key] = $path;
+            return true;
+        }
+        return false;
+    }
+    /**
+     * @param $path
+     * @return string
+     */
+    public function getRelPath($path) {
+        return ltrim(substr($path, strlen($this->path)), '\\\/');
+    }
+    /**
+     * @param $url
+     * @return string
+     */
+    public function getIdFromUrl($url) {
+        $parts = preg_split('~/~', $url, -1, PREG_SPLIT_NO_EMPTY);
+        return implode('.', array_slice($parts, count($parts) >= 2 ? -2 : -1));
+    }
+    /**
+     * @param $src
+     * @param null $fingerprint
+     * @param string $prefix
+     * @param string $suffix
+     * @return string
+     */
+    public function createKey($src, $fingerprint = null, $prefix = 'io', $suffix = 'file') {
+        return sprintf('%s.%s%s%s', substr(hash('sha1', $src), 0, 8), $prefix, $this->pad($src, $fingerprint), $this->pad($src, $suffix, 3));
+    }
+    /**
+     * @param $key
+     * @param $contents
+     */
+    public function put($key, $contents) {
+        if(false === $this->has($key)) {
+            $this->files->dumpFile($this->realizeDir($key), $contents);
+        }
+    }
+    /**
+     * @return void
+     */
+    public function purge() {
+        try {
+            foreach(new FilesystemIterator($this->path, FilesystemIterator::SKIP_DOTS) as $file) {
+                $this->files->remove($file);
+            }
+        } catch(\Exception $e) {
+        }
+    }
+    /**
+     * @param $key
+     */
+    public function delete($key) {
+        $id = $this->createKey($key);
+        $dir = substr($id, 0, strpos($id, '.'));
+        if($this->files->exists($dir = $this->path . '/' . $dir)) {
+            $this->files->remove($dir);
+        }
+    }
+    /**
+     * @param $id
+     * @return string
+     */
+    protected function getFilePath($id) {
+        return sprintf('%s/%s', $this->path, $id);
+    }
+    /**
+     * @param  string $key the cache key
+     * @return string cache file path
+     */
+    protected function realizeDir($key) {
+        $path = $this->getPath($key);
+        if(!$this->files->exists($dir = dirname($path))) {
+            $this->files->mkdir($dir);
+        }
+        return $path;
+    }
+    /**
+     * @param string $key
+     * @return string
+     */
+    protected function getPath($key) {
+        $parsed = $this->parseKey($key);
+        array_shift($parsed);
+        list ($dir, $file) = $parsed;
+        return sprintf('%s/%s/%s', $this->path, $dir, $file);
+    }
+    /**
+     * @param string $src
+     * @param string $pad
+     * @param int $len
+     * @return string
+     */
+    protected function pad($src, $pad, $len = 16) {
+        return substr(hash('sha1', sprintf('%s%s', $src, $pad)), 0, $len);
+    }
+    /**
+     * @param string $path path to cache directory
+     * @param int $permission octal permission level
+     * @return void
+     */
+    protected function setPath($path, $permission) {
+        if(true !== $this->files->exists($path)) {
+            $this->files->mkdir($path, $permission, true);
+        }
+        $this->path = $path;
+    }
+}
