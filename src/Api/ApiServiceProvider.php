b22be207bf09249d478db0dd9366581d9e6ae3fd
twilroad <269044570@qq.com>
2016-08-29 17:56:30 +0800

    添加Api组件

---
+++ src/Api/ApiServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-08-28 20:44
+ */
+namespace Notadd\Api;
+use Notadd\Foundation\Abstracts\AbstractServiceProvider;
+/**
+ * Class ApiServiceProvider
+ * @package Notadd\Api
+ */
+class ApiServiceProvider extends AbstractServiceProvider {
+    /**
+     * @return void
+     */
+    public function boot() {
+    }
+    /**
+     * @return void
+     */
+    public function register() {
+    }
+}
