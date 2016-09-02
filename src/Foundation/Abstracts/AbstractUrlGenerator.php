ad89e362a70e6c1d155aed7c485e1f8e4f8a98c4
twilroad <269044570@qq.com>
2016-09-03 02:25:20 +0800

    添加路由创建、Session认证及Cookie记录的相关类

---
+++ src/Foundation/Abstracts/AbstractUrlGenerator.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-08-28 23:16
+ */
+namespace Notadd\Foundation\Abstracts;
+use Notadd\Foundation\Application;
+use Notadd\Routing\RouteCollector;
+/**
+ * Class AbstractUrlGenerator
+ * @package Notadd\Foundation\Abstracts
+ */
+abstract class AbstractUrlGenerator {
+    /**
+     * @var \Notadd\Foundation\Application
+     */
+    protected $application;
+    /**
+     * @var string
+     */
+    protected $path;
+    /**
+     * @var \Notadd\Routing\RouteCollector
+     */
+    protected $route;
+    /**
+     * AbstractUrlGenerator constructor.
+     * @param \Notadd\Foundation\Application $application
+     * @param \Notadd\Routing\RouteCollector $route
+     */
+    public function __construct(Application $application, RouteCollector $route) {
+        $this->application = $application;
+        $this->route = $route;
+    }
+    /**
+     * @return string
+     */
+    public function toBase() {
+        return $this->path;
+    }
+    /**
+     * @param $path
+     * @return string
+     */
+    public function toPath($path) {
+        return $this->toBase() . '/' . $path;
+    }
+    /**
+     * @param $name
+     * @param array $parameters
+     * @return string
+     */
+    public function toRoute($name, $parameters = []) {
+        $path = $this->route->getPath($name, $parameters);
+        $path = ltrim($path, '/');
+        return $this->toBase() . '/' . $path;
+    }
+}
---
+++ src/Foundation/Http/Middlewares/RememberFromCookie.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-08-31 14:30
+ */
+namespace Notadd\Foundation\Http\Middlewares;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Zend\Stratigility\MiddlewareInterface;
+/**
+ * Class RememberFromCookie
+ * @package Notadd\Foundation\Http\Middlewares
+ */
+class RememberFromCookie implements MiddlewareInterface {
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param callable|null $out
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke(Request $request, Response $response, callable $out = null) {
+        return $out ? $out($request, $response) : $response;
+    }
+}
