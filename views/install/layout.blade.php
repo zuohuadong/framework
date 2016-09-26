652aedf6bad89cd8ab510b7addb3ee8d9d6dc7aa
twilroad <269044570@qq.com>
2016-09-26 17:26:26 +0800

    添加页面信息

--- views/install/layout.blade.php
+++ views/install/layout.blade.php
@@ -2,9 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <title>安装 - Notadd</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Notadd安装页面">
+    <meta name="keyword" content="Notadd,安装,install">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>安装信息</title>
     <link href="{{ asset('assets/install/sheets/bootstrap.min.css') }}" rel="stylesheet">
     @yield('sheets')
 </head>
--- views/install/sucess.blade.php
+++ views/install/sucess.blade.php
@@ -11,17 +11,11 @@
             <div class="container">
                 <div class="error-panel">
                     <ul class="install-info">
-                        <li>
-                            <label for="address">后台管理地址：</label><a href="#"><span id="address">http://www.aaa.com/admin</span></a>
-                        </li>
-                        <li><label for="index">前台首页：</label><a href="#"><span id="index">http://www.aaa.com</span></a>
-                        </li>
-                        <li class="finish"><label for="account">账号：</label><span id="account">admin@email.com</span>
-                        </li>
+                        <li><label for="address">后台管理地址：</label><a href="#"><span id="address">http://www.aaa.com/admin</span></a></li>
+                        <li><label for="index">前台首页：</label><a href="#"><span id="index">http://www.aaa.com</span></a></li>
+                        <li class="finish"><label for="account">账号：</label><span id="account">admin@email.com</span></li>
                         <li class="finish"><label for="psw">密码：</label><span id="psw">您设定的密码</span></li>
-                        <li class="finish">
-                            <label for="help">获取帮助：</label><a href="https://www.notadd.com"><span id="help" style="color: #1BC9A8">点此获取帮助</span></a>
-                        </li>
+                        <li class="finish"><label for="help">获取帮助：</label><a href="https://www.notadd.com"><span id="help" style="color: #1BC9A8">点此获取帮助</span></a></li>
                     </ul>
                 </div>
             </div>
